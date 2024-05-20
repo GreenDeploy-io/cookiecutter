ad72be9696f72cafdedbaa9fb6971d4cf67deb69
Joao Fernandes <ricardo.jfernandes27@gmail.com>
2024-05-20 09:37:42 +0100

    Init version checking on __init__.py needs to acess text file, changed to static value

--- cookiecutter/__init__.py
+++ cookiecutter/__init__.py
@@ -5,9 +5,7 @@ from pathlib import Path
 
 def _get_version() -> str:
     """Read VERSION.txt and return its contents."""
-    path = Path(__file__).parent.resolve()
-    version_file = path / "VERSION.txt"
-    return version_file.read_text(encoding="utf-8").strip()
+    return "2.6.0"
 
 
 __version__ = _get_version()
